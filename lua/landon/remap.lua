56ffb0729fea7cb18db4b1a2c1de512803223f48
LandonHarter <landonharter@outlook.com>
2026-02-02 17:54:13 -0500

    update to work with tmux better

--- lua/landon/remap.lua
+++ lua/landon/remap.lua
@@ -1,7 +1,1 @@
 vim.g.mapleader = " "
-
-vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
-
-vim.keymap.set("n", "<C-t>", ":vsplit<CR><C-w>l:terminal<CR>")
-vim.keymap.set('n', '<C-o>', "<C-w>w")
-vim.keymap.set("t", "<C-o>", "<C-\\><C-n><C-w>w")
